1b5445b1d2ce0096f94f1fcb6da8ea59964d522c
Jackson Meyer <jackson.meyer7@gmail.com>
2016-09-16 22:42:38 -0700

    add all working tests, I am still working on template routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,55 +12,53 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("words", Word.all());
-    //   model.put("template", "templates/index.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   String name = request.queryParams("user-word");
-    //   Word newWord = new Word(name);
-    //   model.put("words", Word.all());
-    //   model.put("template", "templates/index.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine(();
-    //
-    // get("/word/new", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/word-form.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/word/:id", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   word word = word.find(Integer.parseInt(request.params(":id")));
-    //   model.put("word", word);
-    //   model.put("template", "templates/word.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/word/:id", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   word word = word.find(Integer.parseInt(request.params(":id")));
-    //
-    //   String name = request.queryParams("definition");
-    //   definition newDefinition = new definition(name);
-    //   word.addDefinition(newDefinition);
-    //   model.put("word", word);
-    //   model.put("template", "templates/word.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("word/:id/definition/new", (request, response) -> {
-    //     Map<String, Object> model = new HashMap<String, Object>();
-    //     word word = word.find(Integer.parseInt(request.params(":id")));
-    //     model.put("word", word);
-    //     model.put("template", "templates/word-definition-form.vtl");
-    //     return new ModelAndView(model, layout);
-    //   }, new VelocityTemplateEngine();
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Word.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
+    post("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("user-word");
+      Word newWord = new Word(name);
+      model.put("words", Word.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+    get("/word/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/word-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/word/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", word);
+      model.put("template", "templates/word-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/word/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      String name = request.queryParams("definition");
+      Definition newDefinition = new Definition(name);
+      word.addDefinition(newDefinition);
+      model.put("word", word);
+      model.put("template", "templates/word-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("word/:id/definition/new", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        Word word = Word.find(Integer.parseInt(request.params(":id")));
+        model.put("word", word);
+        model.put("template", "templates/word-definition-form.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -7,50 +7,42 @@ public class DefinitionTest {
 
   @Test
   public void Definition_instantiatesNewDefinition_true() {
-    Definition myDefinition = new Definition("anything");
+    Definition myDefinition = new Definition("really tall");
     assertEquals(true, myDefinition instanceof Definition);
   }
 
   @Test
   public void Definition_getName() {
-    Definition myDefinition = new Definition("Megadeth: Rust in Peace");
-    assertEquals("Megadeth: Rust in Peace", myDefinition.getName());
+    Definition myDefinition = new Definition("happy beyond belief");
+    assertEquals("happy beyond belief", myDefinition.getName());
   }
 
   @Test
-  public void Definition_sortedByName() {
-    ArrayList<Definition> newDefinition = new ArrayList<Definition>();
-    Definition myDefinition = new Definition("Megadeth: Rust in Peace");
-    newDefinition.add(myDefinition);
-    myDefinition = new Definition("bob");
-    newDefinition.add(myDefinition);
-    Collections.sort(newDefinition, Definition.DefinitionSort);
-  }
-  @Test
   public void all_returnsAllInstancesOfDefinition_true() {
     Definition firstDefinition = new Definition("sky is the limit");
-    Definition secondDefinition = new Definition("yellowtown");
+    Definition secondDefinition = new Definition("this is a type of archive");
     assertEquals(true, Definition.all().contains(firstDefinition));
     assertEquals(true, Definition.all().contains(secondDefinition));
   }
 
   @Test
   public void clear_emptiesAllDefinitionFromArrayList_0() {
-    Definition myDefinition = new Definition("sky is the limit");
+    Definition myDefinition = new Definition("very round");
     Definition.clear();
     assertEquals(Definition.all().size(), 0);
   }
 
   @Test
   public void getId_DefinitionInstantiateWithAnID_1() {
-    Definition.clear();  // Remember, the test will fail without this line! We need to empty leftover Tasks from previous tests!
-    Definition myDefinition = new Definition("sky is the limit");
+    Definition.clear();
+    Definition myDefinition = new Definition("very thoughtful");
     assertEquals(1, myDefinition.getId());
   }
 
+  @Test
   public void find_returnsDefinitionWithSameId_secondTask() {
-    Definition firstDefinition = new Definition("sky is the limit");
-    Definition secondDefinition = new Definition("yellowtown");
+    Definition firstDefinition = new Definition("difficult to handle");
+    Definition secondDefinition = new Definition("having fun");
     assertEquals(Definition.find(secondDefinition.getId()), secondDefinition);
    }
 }
